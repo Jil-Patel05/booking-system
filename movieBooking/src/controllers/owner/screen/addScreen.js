0cec890187cb0e161c16587430c6fcf1c731af74
Jil-Patel05 <jilpatel2105@gmail.com>
2025-03-16 15:44:31 +0530

    addscreen controller done

--- movieBooking/src/controllers/owner/screen/addScreen.js
+++ movieBooking/src/controllers/owner/screen/addScreen.js
@@ -1,12 +1,59 @@
 const { STATUS_CODE } = require("../../../common/common");
 const ErrorHandler = require("../../../helpers/errorHandler");
-const { insetAndUpdateQuery } = require("../../../helpers/mysqlQuery");
 const sentense = require("../../../common/en-US.json");
 const { pool } = require("../../../config/mysqlDbConnection");
 
-const addScreen = async (req, res, next) => {
-  const screenReq = req.body;
-  const totalNumberOfScreen = screenReq.screens.length;
+const calculateScreenIds = (screenReq, firstInsertedId) => {
+  let id = firstInsertedId;
+  const rowInfo = [];
+  screenReq.screens.forEach((screen) => {
+    screen.rowsInfo.forEach((row) => {
+      const rowData = {
+        screenId: firstInsertedId,
+        rowName: row.rowName,
+        noOfRowSeat: row.noOfRowSeat,
+        rowType: row.rowType,
+        statusId: row.statusId,
+        seatStartFrom: row.seatStartFrom,
+        seatEndTo: row.seatEndTo,
+      };
+      rowInfo.push(rowData);
+    });
+    id++;
+  });
+  return rowInfo;
+};
+
+const calculateRowIds = (
+  screenReq,
+  cinemaId,
+  screenLastInsertedId,
+  rowLastInsertedId
+) => {
+  let screenIdStartFrom = screenLastInsertedId;
+  let rowIdStartFrom = rowLastInsertedId;
+  const seatInfo = [];
+  const seatStatusId = 1;
+  screenReq.screens.forEach((screen) => {
+    screen.rowsInfo.forEach((row) => {
+      for (let i = row.seatStartFrom; i <= row.seatEndTo; i++) {
+        const seatData = {
+          rowId: rowIdStartFrom,
+          screenId: screenIdStartFrom,
+          cinemaId: cinemaId,
+          seatName: i,
+          statusId: seatStatusId,
+        };
+        seatInfo.push(seatData);
+      }
+      rowIdStartFrom++;
+    });
+    screenIdStartFrom++;
+  });
+  return seatInfo;
+};
+
+const prepareScreenData = (screenReq) => {
   const screen = [];
   screenReq.screens.forEach((item) => {
     const screenInfo = {
@@ -18,9 +65,21 @@ const addScreen = async (req, res, next) => {
     screen.push(screenInfo);
   });
   const screenData = JSON.stringify(screen);
+  return screenData;
+};
+
+const isInsertionValid = (row) => {
+  return row && row[0][0] && row[0][0].lastInsertedId;
+};
+
+const addScreen = async (req, res, next) => {
+  const screenReq = req.body;
+  const totalNumberOfScreen = screenReq.screens.length;
+  const screenData = prepareScreenData(screenReq);
   const connection = await pool.getConnection();
   await connection.beginTransaction();
   try {
+    let isAllInsertionDone = false;
     const query = "call add_screenInfo(?,?,?,?)";
     const params = [
       screenReq.cinemaId,
@@ -29,43 +88,42 @@ const addScreen = async (req, res, next) => {
       screenData,
     ];
 
-    const [row] = await connection.execute(query, params);
+    const [screen] = await connection.execute(query, params);
 
-    if (row && row[0][0] && row[0][0].lastInsertedId) {
-      console.log("added");
-      let firstInsertedId = row[0][0].lastInsertedId;
-      const rowInfo = [];
-      screenReq.screens.forEach((screen) => {
-        screen.rowsInfo.forEach((row) => {
-          const rowData = {
-            screenId: firstInsertedId,
-            rowName: row.rowName,
-            noOfRowSeat: row.noOfRowSeat,
-            rowType: row.rowType,
-            statusId: row.statusId,
-            seatStartFrom: row.seatStartFrom,
-            seatEndTo: row.seatEndTo,
-          };
-          rowInfo.push(rowData);
-        });
-        firstInsertedId++;
-      });
-      const rowQuery = "call add_rowInf(?)";
-      const [rowData] = await connection.execute(rowQuery, [
+    if (isInsertionValid(screen)) {
+      const rowInfo = calculateScreenIds(
+        screenReq,
+        screen[0][0].lastInsertedId
+      );
+      const rowQuery = "call add_rowInfo(?)";
+
+      const [row] = await connection.execute(rowQuery, [
         JSON.stringify(rowInfo),
       ]);
-      if (rowData && rowData[0][0] && rowData[0][0].lastInsertedId) {
-        connection.commit();
-        res.status(STATUS_CODE.SUCCESS).json({
-          message: "Screen added successfully",
-        });
-      } else {
-        throw new ErrorHandler(
-          sentense["screen-not-added"],
-          STATUS_CODE.CLIENT_BAD_REQUEST
+
+      if (isInsertionValid(row)) {
+        const seatInfo = calculateRowIds(
+          screenReq,
+          screenReq.cinemaId,
+          screen[0][0].lastInsertedId,
+          row[0][0].lastInsertedId
         );
+        const rowQuery = "call add_seatInfo(?)";
+
+        const [seat] = await connection.execute(rowQuery, [
+          JSON.stringify(seatInfo),
+        ]);
+
+        if (isInsertionValid(seat)) {
+          connection.commit();
+          isAllInsertionDone = true;
+          res.status(STATUS_CODE.SUCCESS).json({
+            message: "Screen added successfully",
+          });
+        }
       }
-    } else {
+    }
+    if (!isAllInsertionDone) {
       throw new ErrorHandler(
         sentense["screen-not-added"],
         STATUS_CODE.CLIENT_BAD_REQUEST
@@ -73,7 +131,6 @@ const addScreen = async (req, res, next) => {
     }
   } catch (err) {
     connection.rollback();
-    console.log("connection rolledback");
     next(err);
   } finally {
     connection.release();
